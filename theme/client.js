dee82e4c1a7562d16a3f67ffdc6f841e1bdc8c4b
zml <747765207@qq.com>
2022-11-22 18:19:56 +0800

    workflow 更改文件引入

--- theme/client.js
+++ theme/client.js
@@ -2,7 +2,6 @@ import { defineClientConfig } from '@vuepress/client'
 import pkg from 'view-ui-plus'
 const { Button } = pkg
 import 'view-ui-plus/dist/styles/viewuiplus.css'
-import rightMenu from './components/rightMenu.vue'
 import Layout from './layouts/Layout.vue'
 export default defineClientConfig({
   layouts: {
@@ -10,7 +9,6 @@ export default defineClientConfig({
   },
   enhance({ app, router }) {
     app.component('Button', Button)
-    app.component('rightMenu', rightMenu)
   },
   setup() {},
   rootComponents: [],
