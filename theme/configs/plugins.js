f8b60915d651d70b07b8ad909ce62d7bf646c020
zml <747765207@qq.com>
2022-11-23 10:43:39 +0800

    wip 增加本地主题themePlguinData配置

--- theme/configs/plugins.js
+++ theme/configs/plugins.js
@@ -1,4 +1,11 @@
-import {
-  searchPlugin
-} from '@vuepress/plugin-search'
-export const plugins = [searchPlugin({})]
+import { searchPlugin } from '@vuepress/plugin-search'
+// import { themeDataPlugin } from '@vuepress/plugin-theme-data'
+// import { sidebar } from './nav/sidebar'
+export const plugins = [
+  searchPlugin({}),
+  // themeDataPlugin({
+  //   themeData: {
+  //     sidebar: sidebar,
+  //   },
+  // }),
+]
